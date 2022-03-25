1b3d846e7eaea30f6270efc88164a94151ed6f94
Yayawak <elio.interesante@gmail.com>
2022-03-25 18:54:48 +0700

    add multiple pages to layout-comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
-import Box from './Box';
-import Grid from './Grid';
-import Notification from './Notification';
+import AppHeader from './AppHeader';
+import PageHome from './PageHome';
+import PageAbout from './PageAbout';
 
 function App() {
     return (
-        <section className='app-section'>
-            <div className='app-container'>
-                <h2>Dashboard</h2>
-                <Notification title='hello' description='click to be pro scammer!'
-                    level='warning'/>
-                <Grid columns={3}>
-                    <Box title='ออเดอร์ต่อวัน'>                        <ul>
-                            <li>3 กันยายน = 1,582 orders</li>
-                            <li>2 กันยายน = 1,358 orders</li>
-                            <li>1 กันยายน = 2,024 orders</li>
-                        </ul>
-                    </Box>
-                    
-                </Grid>
-            </div>
-        </section>
+        <div>
+            <AppHeader />
+            <PageHome />
+            <PageAbout />
+        </div>
+        
     );
 }
 
