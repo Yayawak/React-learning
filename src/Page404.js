c1b40f7f8d70b9212aa3478f573a84d7f8807f09
Yayawak <elio.interesante@gmail.com>
2022-03-26 00:45:57 +0700

    Real addNestedRoute because last commit was wrong by forgotting to add other component

---
+++ src/Page404.js
@@ -0,0 +1,10 @@
+import LayoutPage from "./LayoutPage";
+
+export default function Page404 () {
+    return (
+        <LayoutPage>
+            <h2>404 Not Found</h2>
+            <p>หาไม่เจอหรือเธอไม่มี</p>
+        </LayoutPage>
+    )
+}
---
+++ src/PageHome.js
@@ -0,0 +1,11 @@
+import LayoutPage from "./LayoutPage";
+
+function PageHome () {
+    return (
+        <LayoutPage>
+            <h2>Home หน้าแรก</h2>
+        </LayoutPage>
+    )
+}
+
+export default PageHome;
