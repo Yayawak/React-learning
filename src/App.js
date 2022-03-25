0e4d1852fb76442f441d929f5428dad0519fa2c3
Yayawak <elio.interesante@gmail.com>
2022-03-25 19:46:37 +0700

    fineshed ep17 basic route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import AppHeader from './AppHeader';
 import PageHome from './PageHome';
 import PageAbout from './PageAbout';
+import Page404 from './Page404';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
     return (
         <div>
             <AppHeader />
-            <PageHome />
-            <PageAbout />
+            <Routes>
+                <Route path='/' element={<PageHome />} exact/>
+                <Route path='/about' element={<PageAbout />} />   
+                <Route path='*' element={<Page404 />} />
+            </Routes>
         </div>
         
     );
